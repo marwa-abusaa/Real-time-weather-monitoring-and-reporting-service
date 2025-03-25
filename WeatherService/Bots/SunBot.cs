6053d4a6a214006208db5ba33a258dc6f1fb3168
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 17:14:26 +0200

    Create SunBot class implementing IBot with Activate method

---
+++ WeatherService/Bots/SunBot.cs
@@ -0,0 +1,24 @@
+﻿
+using WeatherService.Models;
+
+namespace WeatherService.Bots
+{
+    public class SunBot : IBot
+    {
+        BotConfig botConfig;
+
+        public SunBot(BotConfig botConfig)
+        {
+            this.botConfig = botConfig;
+        }
+
+        public void Activate(WeatherData data)
+        {
+            if (botConfig.Enabled && data.Temperature < botConfig.Threshold)
+            {
+                Console.WriteLine("SunBot activated!");
+                Console.WriteLine(botConfig.Message);
+            }
+        }
+    }
+}
