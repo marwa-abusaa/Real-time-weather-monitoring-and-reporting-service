551992a8ab24668f37814e339b1db551a5142f88
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 17:13:25 +0200

    Create RainBot class implementing IBot with Activate method

---
+++ WeatherService/Bots/RainBot.cs
@@ -0,0 +1,24 @@
+﻿
+using WeatherService.Models;
+
+namespace WeatherService.Bots
+{
+    public class RainBot : IBot
+    {
+        BotConfig botConfig;
+
+        public RainBot(BotConfig botConfig)
+        {
+            this.botConfig = botConfig;
+        }
+
+        public void Activate(WeatherData data)
+        {
+            if(botConfig.Enabled && data.Humidity > botConfig.Threshold)
+            {
+                Console.WriteLine("RainBot activated!");
+                Console.WriteLine(botConfig.Message);
+            }
+        }
+    }
+}
