483daf67344ec10493c29491dc5cded54d541df1
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 16:18:00 +0200

    Create WeatherData class for weather information

---
+++ WeatherService/Models/WeatherData.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WeatherService.Models
+{
+    public class WeatherData
+    {
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+
+    }
+}
