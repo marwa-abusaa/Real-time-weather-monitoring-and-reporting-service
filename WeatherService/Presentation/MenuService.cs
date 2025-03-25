1abc564b678caeab552b5a65aa53c49851f786aa
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 22:59:23 +0200

    Add MenuService class add implement show menu method

---
+++ WeatherService/Presentation/MenuService.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WeatherService.Presentation
+{
+    public class MenuService
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine("\n1. Enter weather data");
+            Console.WriteLine("2. Exit");
+            Console.Write("\nChoose an option: ");
+        }
+    }
+}
