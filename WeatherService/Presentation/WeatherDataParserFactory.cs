163c96f6c27689931340780be4f1d9442779dbe4
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 23:02:21 +0200

    Add WeatherDataParserFactory class and implement DetectDataFormat method to determine format of data received

---
+++ WeatherService/Presentation/WeatherDataParserFactory.cs
@@ -0,0 +1,19 @@
+﻿
+using WeatherService.Parsers;
+
+namespace WeatherService.Presentation
+{
+    public class WeatherDataParserFactory
+    {
+        public IWeatherDataParser? DetectDataFormat(string inputData)
+        {
+            if (inputData.TrimStart().StartsWith("{"))
+                return new JsonWeatherParser();
+
+            if (inputData.TrimStart().StartsWith("<"))
+                return new XmlWeatherParser();
+
+            return null;
+        }
+    }
+}
