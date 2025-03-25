97929545480c5259203457cd8704066d050e7094
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 16:26:13 +0200

    remove unnecessary space

--- WeatherService/Models/WeatherData.cs
+++ WeatherService/Models/WeatherData.cs
@@ -6,6 +6,5 @@ namespace WeatherService.Models
         public string Location { get; set; }
         public double Temperature { get; set; }
         public double Humidity { get; set; }
-
     }
 }
