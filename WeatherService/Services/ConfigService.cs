bb23af1e9824e0092712db2a8773055310716ce6
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 17:18:31 +0200

    Create ConfigService class with LoadConfigSettings method to read from JSON file

---
+++ WeatherService/Services/ConfigService.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.Json;
+using WeatherService.Bots;
+
+namespace WeatherService.Services
+{
+    public class ConfigService
+    {
+        private readonly string _JsonFilePath;
+
+        public ConfigService(string jsonFilePath)
+        {
+            _JsonFilePath = jsonFilePath;
+        }
+
+        public Dictionary<IBot, BotConfig> loadConfigSettings()
+        {
+            string configText = File.ReadAllText(_JsonFilePath);
+            var results = JsonSerializer.Deserialize<Dictionary<IBot,BotConfig>>(configText);
+            return results;
+        }
+    }
+}
