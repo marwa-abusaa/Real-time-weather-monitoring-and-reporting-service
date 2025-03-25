cba00b45bdbb00cd30fac19f7a73b503d09ee873
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 22:09:06 +0200

    Add WeatherNotifier class and implement NotifyBots method to notify bots based on the received data

---
+++ WeatherService/Services/WeatherNotifier.cs
@@ -0,0 +1,24 @@
+﻿
+using WeatherService.Bots;
+using WeatherService.Models;
+
+namespace WeatherService.Services
+{
+    public class WeatherNotifier
+    {
+        private readonly List<IBot> _bots;
+
+        public WeatherNotifier(List<IBot> bots)
+        {
+            _bots = bots;
+        }
+
+        public void NotifyBots(WeatherData data)
+        {
+            foreach (var bot in _bots)
+            {
+                bot.Activate(data);
+            }
+        }
+    }
+}
