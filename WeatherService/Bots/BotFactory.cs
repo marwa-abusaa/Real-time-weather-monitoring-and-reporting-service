3040e8f74e4e7c6281ce6b7b9fd88b0b1b169a77
marwa-abusaa <s12028718@stu.najah.edu>
2025-03-25 21:56:02 +0200

    Add BotFactory class to create bots

---
+++ WeatherService/Bots/BotFactory.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WeatherService.Bots
+{
+    public class BotFactory
+    {
+        private readonly Dictionary<string, BotConfig> _botConfigurations;
+
+        public BotFactory(Dictionary<string, BotConfig> botConfigurations)
+        {
+            _botConfigurations = botConfigurations;
+        }
+        
+    }
+}
