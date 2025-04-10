d0ae6c44f54dcf74dd62e66e2fc99ea9dd54dc49
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-10 19:20:05 +0200

    Add WeatherDataParserFactoryTests class to verfiy Create correctParser

--- WeatherService.Tests/WeatherDataParserFactoryTests.cs
+++ WeatherService.Tests/WeatherDataParserFactoryTests.cs
@@ -1,47 +1,45 @@
-﻿
-using FluentAssertions;
+﻿using FluentAssertions;
 using WeatherService.Services;
 using WeatherService.Services.Parsers;
 
-namespace WeatherService.Tests
-{
-    public class WeatherDataParserFactoryTests
-    {
-        [Theory]
-        [InlineData("""           
-             {
-             "Location": "City Name",
-             "Temperature": 23.0,
-             "Humidity": 85.0
-             }            
-            """ , typeof(JsonWeatherParser)
-            )]
+namespace WeatherService.Tests;
 
-        [InlineData(@"           
+public class WeatherDataParserFactoryTests
+{
+    [Theory]
+    [InlineData("""           
+         {
+         "Location": "City Name",
+         "Temperature": 23.0,
+         "Humidity": 85.0
+         }            
+        """ , typeof(JsonWeatherParser)
+        )]
+
+    [InlineData(@"           
              <WeatherData>
              <Location>City Name</Location>
              <Temperature>23.0</Temperature>
              <Humidity>85.0</Humidity>
              </WeatherData>           
             ", typeof(XmlWeatherParser)
-            )]
+        )]
 
-        [InlineData("Invalid Data", null)]
+    [InlineData("Invalid Data", null)]
 
-        public void Create_ShouldReturnCorrectParserBasedOnInputData(string inputData, Type expectedReturnedType)
+    public void Create_ShouldReturnCorrectParserBasedOnInputData(string inputData, Type expectedReturnedType)
+    {
+        var weatherDataParserFactory = new WeatherDataParserFactory();
+
+        var result = weatherDataParserFactory.Create(inputData);
+
+        if (expectedReturnedType == null)
         {
-            var weatherDataParserFactory = new WeatherDataParserFactory();
-
-            var result = weatherDataParserFactory.Create(inputData);
-
-            if (expectedReturnedType == null)
-            {
-                result.Should().BeNull();
-            }
-            else
-            {
-                result.Should().BeOfType(expectedReturnedType);
-            }           
+            result.Should().BeNull();
         }
+        else
+        {
+            result.Should().BeOfType(expectedReturnedType);
+        }           
     }
 }
