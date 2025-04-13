6502ea7a9a92391a301b017cfa480e4a0956a948
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-13 12:54:47 +0300

    add SnowBotTests class to test Activate method

---
+++ WeatherService.Tests/SnowBotTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using WeatherService.Models;
+using WeatherService.Services.Bots;
+
+namespace WeatherService.Tests;
+
+public class SnowBotTests
+{
+    [Theory]
+    [InlineData(false, 80, 3, "", false)]
+    [InlineData(true, 20, 3, "", false)]
+    [InlineData(true, 0, 3, "Brrr, it's getting chilly!", true)]
+    public void Activate_ShouldBehaveAccordingToConditions(
+        bool enabled,
+        double temperature,
+        double threshold,
+        string message,
+        bool shouldPrint)
+    {
+        var fixture = new Fixture();
+        WeatherData weatherData = fixture.Build<WeatherData>().With(w => w.Temperature, temperature).Create();
+        var botConfig = new BotConfig
+        {
+            Enabled = enabled,
+            Threshold = threshold,
+            Message = message
+        };
+
+        SnowBotService snowBotService = new SnowBotService(botConfig);
+        snowBotService.Activate(weatherData);
+
+
+        if (shouldPrint)
+        {
+            Assert.Equal("Brrr, it's getting chilly!", message);
+        }
+        else
+        {
+            Assert.Equal("", message);
+        }
+
+    }
+}
