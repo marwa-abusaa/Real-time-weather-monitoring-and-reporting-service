fdfa81f684af1d5cf80b71040ff9c51888ed60b5
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-13 12:58:14 +0300

    add SunBotTests class to test Activate method

---
+++ WeatherService.Tests/SunBotTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using WeatherService.Models;
+using WeatherService.Services.Bots;
+
+namespace WeatherService.Tests;
+
+public class SunBotTests
+{
+    [Theory]
+    [InlineData(false, 80, 70, "", false)]
+    [InlineData(true, 60, 70, "", false)]
+    [InlineData(true, 80, 70, "It looks like it's about to pour down!", true)]
+    public void Activate_ShouldBehaveAccordingToConditions(
+        bool enabled,
+        double humidity,
+        double threshold,
+        string message,
+        bool shouldPrint)
+    {
+        var fixture = new Fixture();
+        WeatherData weatherData = fixture.Build<WeatherData>().With(w => w.Humidity, humidity).Create();
+        var botConfig = new BotConfig
+        {
+            Enabled = enabled,
+            Threshold = threshold,
+            Message = message
+        };
+
+        RainBotService rainBotService = new RainBotService(botConfig);
+        rainBotService.Activate(weatherData);
+
+
+        if (shouldPrint)
+        {
+            Assert.Equal("It looks like it's about to pour down!", message);
+        }
+        else
+        {
+            Assert.Equal("", message);
+        }
+
+    }
+}
