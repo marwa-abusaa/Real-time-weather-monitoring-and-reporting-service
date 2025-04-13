316890bed0bc7ccb22132f537c39ca361fb1edee
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-13 14:59:37 +0300

    add BotFactoryTests class to test CreateBotList method

---
+++ WeatherService.Tests/BotFactoryTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using WeatherService.Bots;
+using WeatherService.Models;
+
+namespace WeatherService.Tests;
+
+public class BotFactoryTests
+{
+    private readonly Fixture _fixture;
+    private readonly BotConfig _botConfig;
+
+    public BotFactoryTests()
+    {
+        _fixture = new Fixture();
+        _botConfig = _fixture.Create<BotConfig>();
+    }
+
+
+    [Theory]
+    [InlineData("RainBot")]
+    [InlineData("SnowBot")]
+    [InlineData("SunBot")]
+    public void CreateBotList_ShouldReturnListOfBots_WhenBotTypesAreValid(string validBot)
+    {       
+        var bots = new Dictionary<string, BotConfig>
+        {
+            { validBot, _botConfig }
+        };
+        var botFactory = new BotFactory(bots);
+
+        var validBots = botFactory.CreateBotList();
+
+        validBots.Should().ContainSingle(bot => bot is IBotService);
+    }
+
+
+    [Fact]
+    public void CreateBotList_ShouldThrowInvalidOperationException_WhenUnknownBotTypeIsProvided()
+    {       
+        var bots = new Dictionary<string, BotConfig>
+        {
+            { "UnknownBot", _botConfig }
+        };
+        var botFactory = new BotFactory(bots);
+
+        Assert.Throws<InvalidOperationException>(() => botFactory.CreateBotList());
+    }
+}
