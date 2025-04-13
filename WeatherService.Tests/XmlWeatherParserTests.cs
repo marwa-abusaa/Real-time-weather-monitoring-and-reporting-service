a61e7b205f8c5a514f2c26dd5ba652bdeadc3b5f
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-13 13:25:24 +0300

    add XmlWeatherParserTests class to test Parse method

---
+++ WeatherService.Tests/XmlWeatherParserTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using WeatherService.Services.Parsers;
+
+namespace WeatherService.Tests;
+
+public class XmlWeatherParserTests
+{
+    [Fact]
+    public void Parse_ShouldDeserializeValidXml_ToWeatherData()
+    {
+        var validInputJson = @"           
+             <WeatherData>
+             <Location>City Name</Location>
+             <Temperature>23.0</Temperature>
+             <Humidity>85.0</Humidity>
+             </WeatherData>           
+            ";
+
+        var xmlWeatherParser = new XmlWeatherParser();
+        var result = xmlWeatherParser.Parse(validInputJson);
+
+
+        result.Location.Should().Be("City Name");
+        result.Temperature.Should().Be(23.0);
+        result.Humidity.Should().Be(85.0);
+    }
+}
