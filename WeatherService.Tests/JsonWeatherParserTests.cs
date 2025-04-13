136fe868355d29714b934d6f4e709414ade702bc
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-13 13:21:54 +0300

    add JsonWeatherParserTests class to test Parse method

---
+++ WeatherService.Tests/JsonWeatherParserTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using WeatherService.Services.Parsers;
+
+namespace WeatherService.Tests;
+
+public class JsonWeatherParserTests
+{
+    [Fact]
+    public void Parse_ShouldDeserializeValidJson_ToWeatherData()
+    {
+        var validInputJson = """
+        {
+        "Location": "Tulkarem",
+        "Temperature": 23.0,
+        "Humidity": 85.0
+        }
+        """;         
+
+        var jsonWeatherParser = new JsonWeatherParser();
+        var result = jsonWeatherParser.Parse(validInputJson);
+
+
+        result.Location.Should().Be("Tulkarem");
+        result.Temperature.Should().Be(23.0);
+        result.Humidity.Should().Be(85.0);
+    }
+}
