3ae95aa6fd0158f44cedbe1bb932a6c99dc2845e
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-15 22:24:10 +0300

    refactor RainBotTests to split the test cases into different tests

--- WeatherService.Tests/BotsTesting/RainBotTests.cs
+++ WeatherService.Tests/BotsTesting/RainBotTests.cs
@@ -1,43 +1,67 @@
-﻿using AutoFixture;
-using WeatherService.Models;
+﻿using WeatherService.Models;
 using WeatherService.Services.Bots;
 
 namespace WeatherService.Tests.BotsTesting;
 
 public class RainBotTests
 {
-    [Theory]
-    [InlineData(false, 80, 70, "", false)]
-    [InlineData(true, 60, 70, "", false)]
-    [InlineData(true, 80, 70, "It looks like it's about to pour down!", true)]
-    public void Activate_ShouldBehaveAccordingToConditions(
-        bool enabled,
-        double humidity,
-        double threshold,
-        string message,
-        bool shouldPrint)
+    [Fact]
+    public void Activate_ShouldReturnTrue_WhenEnabledAndHumidityAboveThreshold()
     {
-        var fixture = new Fixture();
-        WeatherData weatherData = fixture.Build<WeatherData>().With(w => w.Humidity, humidity).Create();
-        var botConfig = new BotConfig
+        // Arrange
+        var config = new BotConfig
         {
-            Enabled = enabled,
-            Threshold = threshold,
-            Message = message
+            Enabled = true,
+            Threshold = 70,
+            Message = "It looks like it's about to pour down!"
         };
+        var data = new WeatherData { Humidity = 80 };
+        var service = new RainBotService(config);
 
-        RainBotService rainBotService = new RainBotService(botConfig);
-        rainBotService.Activate(weatherData);
+        // Act
+        var result = service.Activate(data);
 
+        // Assert
+        Assert.True(result);
+    }
 
-        if (shouldPrint)
+    [Fact]
+    public void Activate_ShouldReturnFalse_WhenNotEnabled()
+    {
+        // Arrange
+        var config = new BotConfig
         {
-            Assert.Equal("It looks like it's about to pour down!", message);
-        }
-        else
-        {
-            Assert.Equal("", message);
-        }
+            Enabled = false,
+            Threshold = 70,
+            Message = "RainBot isn't activated."
+        };
+        var data = new WeatherData { Humidity = 80 };
+        var service = new RainBotService(config);
 
+        // Act
+        var result = service.Activate(data);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void Activate_ShouldReturnFalse_WhenHumidityBelowThreshold()
+    {
+        // Arrange
+        var config = new BotConfig
+        {
+            Enabled = true,
+            Threshold = 70,
+            Message = "RainBot isn't activated. Humidity is below threshold."
+        };
+        var data = new WeatherData { Humidity = 60 };
+        var service = new RainBotService(config);
+
+        // Act
+        var result = service.Activate(data);
+
+        // Assert
+        Assert.False(result);
     }
 }
