81a6081ba8d4ee7488c6613216abbd9f62f76d1b
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-15 23:10:40 +0300

    add two new test method for sad scenario

--- WeatherService.Tests/ParsersTesting/JsonWeatherParserTests.cs
+++ WeatherService.Tests/ParsersTesting/JsonWeatherParserTests.cs
@@ -1,14 +1,25 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Text.Json;
 using WeatherService.Services.Parsers;
 
 namespace WeatherService.Tests.ParsersTesting;
 
 public class JsonWeatherParserTests
 {
-    [Fact]
-    public void Parse_ShouldDeserializeValidJson_ToWeatherData()
+    private readonly JsonWeatherParser jsonWeatherParser;
+
+    public JsonWeatherParserTests()
     {
-        var validInputJson = """
+        jsonWeatherParser = new JsonWeatherParser();
+    }
+
+
+    [Fact]
+    public void Parse_ValidJsonInput_ShouldDeserializeToWeatherData()
+    {
+        //Arrange
+        const string validInputJson = """
         {
         "Location": "Tulkarem",
         "Temperature": 23.0,
@@ -16,11 +27,33 @@ public class JsonWeatherParserTests
         }
         """;
 
-        var jsonWeatherParser = new JsonWeatherParser();
+        //Act
         var result = jsonWeatherParser.Parse(validInputJson);
 
-        result.Location.Should().Be("Tulkarem");
-        result.Temperature.Should().Be(23.0);
-        result.Humidity.Should().Be(85.0);
+        //Assert
+        using (new AssertionScope())
+        {
+            result.Location.Should().Be("Tulkarem");
+            result.Temperature.Should().Be(23.0);
+            result.Humidity.Should().Be(85.0);
+        }
     }
+
+    [Fact]
+    public void Parse_InvalidJson_ThrowsJsonException()
+    {
+        // Arrange
+        const string invalidJson = "{ this is not valid JSON }";
+
+        // Act & Assert
+        Assert.Throws<JsonException>(() => jsonWeatherParser.Parse(invalidJson));
+    }
+
+    [Fact]
+    public void Parse_NullInput_ThrowsArgumentNullException()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => jsonWeatherParser.Parse(null!));
+    }
+
 }
