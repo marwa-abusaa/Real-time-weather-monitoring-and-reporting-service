6ae7d598b20c8f8b9a6bef565f518acdbd347924
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-15 23:16:11 +0300

    add two new test method for sad scenario

--- WeatherService.Tests/ParsersTesting/XmlWeatherParserTests.cs
+++ WeatherService.Tests/ParsersTesting/XmlWeatherParserTests.cs
@@ -1,14 +1,23 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Xml;
 using WeatherService.Services.Parsers;
 
 namespace WeatherService.Tests.ParsersTesting;
 
 public class XmlWeatherParserTests
 {
-    [Fact]
-    public void Parse_ShouldDeserializeValidXml_ToWeatherData()
+    private readonly XmlWeatherParser xmlWeatherParser;
+
+    public XmlWeatherParserTests()
     {
-        var validInputJson = @"           
+        xmlWeatherParser = new XmlWeatherParser();
+    }
+    [Fact]
+    public void Parse_ValidXmlInput_ShouldDeserializeToWeatherData()
+    {
+        //Arrange
+        const string validInputJson = @"           
              <WeatherData>
              <Location>City Name</Location>
              <Temperature>23.0</Temperature>
@@ -16,12 +25,32 @@ public class XmlWeatherParserTests
              </WeatherData>           
             ";
 
-        var xmlWeatherParser = new XmlWeatherParser();
+        //Act
         var result = xmlWeatherParser.Parse(validInputJson);
 
-
+        //Assert
+        using(new AssertionScope())
+        {
         result.Location.Should().Be("City Name");
         result.Temperature.Should().Be(23.0);
         result.Humidity.Should().Be(85.0);
+        }
+    }
+
+    [Fact]
+    public void Parse_InvalidXml_ThrowsXmlException()
+    {
+        // Arrange
+        const string invalidXml = "<this is not valid JSON>";
+
+        // Act & Assert
+        Assert.Throws<XmlException>(() => xmlWeatherParser.Parse(invalidXml));
+    }
+
+    [Fact]
+    public void Parse_NullInput_ThrowsArgumentNullException()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => xmlWeatherParser.Parse(null!));
     }
 }
