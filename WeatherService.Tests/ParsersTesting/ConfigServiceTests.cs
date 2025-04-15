b2f283bc39566e39ebfbf08ea4b11976c1326118
marwa-abusaa <s12028718@stu.najah.edu>
2025-04-15 22:57:09 +0300

    add LoadConfigSettings_InvalidJson_ThrowsJsonException method for sad scenario

--- WeatherService.Tests/ParsersTesting/ConfigServiceTests.cs
+++ WeatherService.Tests/ParsersTesting/ConfigServiceTests.cs
@@ -2,15 +2,25 @@
 using WeatherService.Models;
 using WeatherService.Services.FileReaders;
 using FluentAssertions;
+using System.Text.Json;
 
 namespace WeatherService.Tests.ParsersTesting;
 
 public class ConfigServiceTests
 {
+    private readonly Mock<IFileReader> mockFileReader;
+
+    public ConfigServiceTests()
+    {
+        mockFileReader = new Mock<IFileReader>();
+    }
+
+
     [Fact]
     public void LoadConfigSettings_ValidJsonInput_ShouldReturnDeserializedDictionary()
     {
-        var fakeJsonConfig = """
+        // Arrange
+        const string fakeJsonConfig = """
             {
             "RainBot": {
               "Enabled": true,
@@ -19,17 +29,30 @@ public class ConfigServiceTests
               }
             }                
             """;
-        var mockFileReader = new Mock<IFileReader>();
+
         mockFileReader.Setup(x => x.ReadAllText(It.IsAny<string>())).Returns(fakeJsonConfig);
         var configService = new ConfigService("botsSettings.json", mockFileReader.Object);
 
-
+        //Act
         var results = configService.loadConfigSettings();
 
-
+        //Assert
         results.Should().ContainKey("RainBot");
         results["RainBot"].Enabled.Should().Be(true);
         results["RainBot"].Threshold.Should().Be(70);
         results["RainBot"].Message.Should().Be("It looks like it's about to pour down!");
+    }
+
+    [Fact]
+    public void LoadConfigSettings_InvalidJson_ThrowsJsonException()
+    {
+        // Arrange
+        const string invalidJson = "This is not JSON!";
+        mockFileReader.Setup(x => x.ReadAllText(It.IsAny<string>())).Returns(invalidJson);
+        var configService = new ConfigService(It.IsAny<string>(), mockFileReader.Object);
+
+
+        // Act & Assert
+        Assert.Throws<JsonException>(() => configService.loadConfigSettings());
     }
 }
